6dc9bcc489a1741eccd081f5fa0eb4a02f9a3627
Himesh  Vats <himeshvats@Himeshs-Mac-mini.local>
2022-02-16 16:51:56 +0530

    Updates

---
+++ precache-manifest.18eb4ed381b23648c6dc21cf35eaeead.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfded03c2a6e527b8c5ccf1ce6e14fe",
+    "url": "/mybook-library/index.html"
+  },
+  {
+    "revision": "1ef376f5551d67f4c84c",
+    "url": "/mybook-library/static/css/main.241c7d39.chunk.css"
+  },
+  {
+    "revision": "39c7ef31340bcd853e86",
+    "url": "/mybook-library/static/js/2.dba28001.chunk.js"
+  },
+  {
+    "revision": "39b2ff43c9f6d0f87d96213c53d39949",
+    "url": "/mybook-library/static/js/2.dba28001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ef376f5551d67f4c84c",
+    "url": "/mybook-library/static/js/main.8c0c9694.chunk.js"
+  },
+  {
+    "revision": "b26c38b315606611aaff",
+    "url": "/mybook-library/static/js/runtime-main.b13cccfa.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/mybook-library/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/mybook-library/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/mybook-library/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
